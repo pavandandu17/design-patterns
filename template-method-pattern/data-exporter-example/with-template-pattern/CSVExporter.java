7bc6d13120c0a897c1818be604302a1fb3ae79e9
Pavan Dandu <pavandandu17@gmail.com>
2026-02-19 01:46:52 +0530

    Data exporter with template method

---
+++ template-method-pattern/data-exporter-example/with-template-pattern/CSVExporter.java
@@ -0,0 +1,6 @@
+public class CSVExporter extends DataExporter {
+    @Override
+    void formatData() {
+        System.out.println("Formatting data to CSV");
+    }
+}
---
+++ template-method-pattern/data-exporter-example/with-template-pattern/PDFExporter.java
@@ -0,0 +1,6 @@
+public class PDFExporter extends DataExporter {
+    @Override
+    void formatData() {
+        System.out.println("Formatting data to PDF");
+    }
+}
